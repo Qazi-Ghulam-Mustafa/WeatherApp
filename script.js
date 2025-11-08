46f8ac07fe49492258464b5176c027708bec8b7a
Qazi-Ghulam-Mustafa <Qazi-Ghulam-Mustafa>
2025-11-08 08:40:14 +0500

    Temorary Commit

--- script.js
+++ script.js
@@ -1,4 +1,132 @@
-const opitions = {
-    method: "Get",
+const cityInput = document.querySelector('.city-input');
+const searchBtn = document.querySelector('.search-btn');
+const notFoundSection = document.querySelector('.not-found');
+const weatherInfoSection = document.querySelector('.Weather-info');
+const searchFoundSection = document.querySelector('.search-city');
+const countryTxt = document.querySelector('.country-txt');
+const tempTxt = document.querySelector('.temp-txt');
+const conditionTxt = document.querySelector('.condition-txt');
+const humidityValueText = document.querySelector('.humidity-value-text');
+const windValueText = document.querySelector('.wind-value-text');
+const weatherSummaryImg = document.querySelector('.weather-summary-img');
+const currentDateText = document.querySelector('.current-date-txt');
+const forecastItemsContainer = document.querySelector('.forecast-item-container');
 
+const apiKey = '02a3cc76172db1d9cd519893f800edcf';
+
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() !== '') {
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && cityInput.value.trim() !== '') {
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(apiUrl);
+    return response.json();
+}
+
+function getWeatherIcon(id) {
+    if (id >= 200 && id <= 232) return 'thunder storm.png';
+    if (id >= 300 && id <= 321) return 'drizzle.png';
+    if (id >= 500 && id <= 531) return 'rain.png';
+    if (id >= 600 && id <= 622) return 'snow.png';
+    if (id === 800) return 'clearSky.png';
+    if (id >= 801 && id <= 804) return 'cloud.png';
+    return 'cloud.png';
+}
+
+function getCurrentDate() {
+    const currentDate = new Date();
+    const options = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short',
+    };
+    return currentDate.toLocaleDateString('en-GB', options);
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city);
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection);
+        return;
+    }
+
+    const {
+        name: cityName,
+        main: { temp, humidity },
+        weather: [{ id, main }],
+        wind: { speed }
+    } = weatherData;
+
+    countryTxt.textContent = cityName;
+    tempTxt.textContent = Math.round(temp) + '℃';
+    conditionTxt.textContent = main;
+    humidityValueText.textContent = humidity + '%';
+    windValueText.textContent = speed + ' m/s';
+    currentDateText.textContent = getCurrentDate();
+    weatherSummaryImg.src = `img/${getWeatherIcon(id)}`;
+
+    await updateForecastsInfo(city);
+    showDisplaySection(weatherInfoSection);
+}
+
+async function updateForecastsInfo(city) {
+    const forecastsData = await getFetchData('forecast', city);
+    if (!forecastsData.list) return;
+
+    const timeTaken = '12:00:00';
+    const todayDate = new Date().toISOString().split('T')[0];
+
+    forecastItemsContainer.innerHTML = '';
+    forecastsData.list.forEach(forecastWeather => {
+        if (
+            forecastWeather.dt_txt.includes(timeTaken) &&
+            !forecastWeather.dt_txt.includes(todayDate)
+        ) {
+            updateForecastItems(forecastWeather);
+        }
+    });
+}
+
+function updateForecastItems(weatherData) {
+    const {
+        dt_txt: date,
+        weather: [{ id }],
+        main: { temp }
+    } = weatherData;
+
+    const dateTaken = new Date(date);
+    const dateOption = {
+        day: '2-digit',
+        month: 'short'
+    };
+    const dateResult = dateTaken.toLocaleDateString('en-US', dateOption);
+
+    const forecastItemHTML = `
+        <div class="forecast-item">
+            <h5 class="forecast-item-date regular-text">${dateResult}</h5>
+            <img src="img/${getWeatherIcon(id)}" alt="" class="forecast-item-img">
+            <h5 class="forecast-item-temp">${Math.round(temp)}℃</h5>
+        </div>
+    `;
+    forecastItemsContainer.insertAdjacentHTML('beforeend', forecastItemHTML);
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchFoundSection, notFoundSection].forEach(
+        s => (s.style.display = 'none')
+    );
+    section.style.display = 'flex';
 }
